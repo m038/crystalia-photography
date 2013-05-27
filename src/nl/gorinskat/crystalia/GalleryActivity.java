a5add2cad1be955b77fae8ce3c9e01ea05be68aa
Mischa Gorinskat <m.gorinskat@gmail.com>
2013-05-27 20:14:12 +0200

    Added quick gallery (hardecoded sizes, not memory optimized loading) Added margin to contentwrapper of most view Set button size to fill_parent for landscape wallpaper activity

--- src/nl/gorinskat/crystalia/GalleryActivity.java
+++ src/nl/gorinskat/crystalia/GalleryActivity.java
@@ -1,6 +1,10 @@
 package nl.gorinskat.crystalia;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class GalleryActivity extends CrystaliaActivity {
 
@@ -10,6 +14,16 @@ public class GalleryActivity extends CrystaliaActivity {
 		setContentView(R.layout.activity_gallery);
 		
 		super.setupActionBar(true);
+
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new GalleryAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(GalleryActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 	}
 
 }
--- src/nl/gorinskat/crystalia/GalleryAdapter.java
+++ src/nl/gorinskat/crystalia/GalleryAdapter.java
@@ -1,11 +1,51 @@
 package nl.gorinskat.crystalia;
 
-/**
- * Created with IntelliJ IDEA.
- * User: mischa
- * Date: 5/20/13
- * Time: 7:13 PM
- * To change this template use File | Settings | File Templates.
- */
-public class GalleryAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class GalleryAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public GalleryAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(800, 800));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.gallery_1, R.drawable.gallery_2,
+            R.drawable.gallery_3, R.drawable.gallery_4,
+            R.drawable.gallery_5, R.drawable.gallery_6
+    };
 }
